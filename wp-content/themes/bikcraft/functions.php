309466e44db424b4aa8194d7fd4b9a2cbc8b5aa9
Rodrigo Viturino <viturino_souza@outlook.com>
2019-02-13 00:37:30 -0200

    Ativando menu do site e alterando a class do css active, adicionando que o wordpress insere no li

--- wp-content/themes/bikcraft/functions.php
+++ wp-content/themes/bikcraft/functions.php
@@ -2,7 +2,7 @@
 
 // Funções para Limpar o Header
 remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link', 10, 0);
+remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 remove_action('wp_head', 'feed_links_extra', 3);
@@ -13,14 +13,21 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_styles', 'print_emoji_styles');
 
 // Habilitar Menus
-add_theme_support('menus');
+add_theme_support('menus'); 
 
-function my_custom_size(){
-    add_imagem_size('large', 1400, 380, true);
-    add_imagem_size('medium', 768, 380, true);
+// Registrar Menu
+function register_my_menu() {
+    register_nav_menu('menu-principal', __('Menu Principal') );
 }
+add_action('init', 'register_my_menu');
 
+function my_custom_sizes(){
+    add_image_size('large', 1400, 380, true);
+    add_image_size('medium', 768, 380, true);
+}
+// Adicionar Ação
 // Essa ação só vai acontecer quando o INICIAR O TEMA
+    // Quando vai acontecer?  // Ativar qual função?
 add_action('after_setup_theme', 'my_custom_sizes');
 
 
--- wp-content/themes/bikcraft/header.php
+++ wp-content/themes/bikcraft/header.php
@@ -30,12 +30,14 @@
 					<img src="<?php echo get_template_directory_uri(); ?>. /img/bikcraft.png" alt="Bikcraft">
 				</a>
 				<nav class="grid-12 header_menu">
-					<ul>
-						<li><a href="/cursos/origamid/wp_bikcraft/Sobre/">Sobre</a></li>
-						<li><a href="/cursos/origamid/wp_bikcraft/produtos/">Produtos</a></li>
-						<li><a href="/cursos/origamid/wp_bikcraft/portfolio/">Portfólio</a></li>
-						<li><a href="/cursos/origamid/wp_bikcraft/contato/">Contato</a></li>
-					</ul>
+					<?php
+						$args = array(
+							'menu' => 'principal',
+							'theme_location' => 'menu-principal',
+							'container' => false
+						);
+						wp_nav_menu( $args );
+					?>
 				</nav>
 			</div>
 		</header>
